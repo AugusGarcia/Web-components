a6d468bdff6e069833bb7bb4336cc237619529d2
AugusGarcia <augustoyt21@gmail.com>
2025-05-01 22:01:58 -0300

    fix errores

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,10 +5,10 @@ function navComponent(el) {
       <nav>
         <div class="nav">
           <a href="/" class="nav__logo">
-            <img src="./src/img/logo-personal.jpg" alt="logo" class="nav__logo-img"/>
+            <img src="../../../img/logo-personal.jpg" alt="logo" class="nav__logo-img"/>
           </a>
           <span class="abrir-ventana">
-            <img src="/src/img/menu.png" alt="menu" class="nav__logo-menu"/>
+            <img src="./src/img/menu.png" alt="menu" class="nav__logo-menu"/>
           </span>
         <div class="nav__desktop">
           <ul class="nav__links">
